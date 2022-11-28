7aa9996753c0db8a1089d64cc2e83f4bcfa31b7f
Oloja Muyiwa <olojam4969@gmail.com>
2022-11-28 15:39:08 +0100

    basic calculator

--- www/index.php
+++ www/index.php
@@ -7,7 +7,28 @@
   <title>Php Starter Project</title>
 </head>
 <body>
-  
+  <form action="index.php" method="post">
+    First Num: <input type="number" step="0.00667787878601" name="num1">
+    Second Num: <input type="number" step="0.00055555554561" name="num2">
+    OP: <input type="textbox" name="op"><br>
+    <input type="submit">
+  </form>
 </body>
-<?php echo "hello world"; ?>
+<?php 
+ $num1 = $_POST["num1"];
+ $num2 = $_POST["num2"];
+ $op = $_POST["op"];
+
+ if($op == "+"){
+  echo ($num1 + $num2);
+ } elseif($op == "-"){
+  echo ($num1 - $num2);
+ } elseif($op == "*"){
+  echo ($num1 * $num2);
+ } elseif($op == "/"){
+  echo ($num1 / $num2);
+ } else{
+  echo ucwords('invalid operator');
+ }
+ ?>
 </html>
